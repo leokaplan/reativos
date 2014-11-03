e2af83216cb6a94865b6f85a915169f8b9ee366f
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2014-11-03 18:18:52 -0200

    intial

---
+++ projeto-final/Lui-grid.lua
@@ -0,0 +1,8 @@
+g1 = {}
+g1.clr_bg       = &_colors.gray;
+g1.clr_fr       = &_colors.red;
+g1.clr_cell_bg  = &_colors.white;
+g1.clr_cell_fr  = &_colors.black;
+g1.spc_x        = 10;
+g1.spc_y        = 10;
+
